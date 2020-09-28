5ef843760e748b7c2b8aa29287fcbd29a3ba6b3e
Fabricio Zanotto <fabricioz.1.2@hotmail.com>
2020-09-28 00:54:22 -0300

    Mudando a cor de fundo da imagem do logo para usar no Header (adicionando cor cinza escuro)

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
-    header.classList.toggle("PassouDaImagem", window.scrollY > 1050);
+    header.classList.toggle("PassouDaImagem", window.scrollY > 1000);
 })
