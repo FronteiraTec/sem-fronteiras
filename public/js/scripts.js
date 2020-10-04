a29808b546d414aa280841cc593294c09d469620
Fabricio Zanotto <fabricioz.1.2@hotmail.com>
2020-10-04 19:29:43 -0300

    adicioando footer, alterando imagem inicial e separando arquivos.css unificados para cada parte

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,5 @@
 window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("PassouDaImagem", window.scrollY > 1000);
+    //passou de 1000px de altura, a classe do header vai para .PassouDaImagem{}
 })
