d606269f8dc9873ad40be2643b8cd2047efd705e
Fabricio Zanotto <fabricioz.1.2@hotmail.com>
2020-10-16 23:22:00 -0300

    Arrumando header conforme pedido na primeira sprint (sem responsividade)

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
-    header.classList.toggle("PassouDaImagem", window.scrollY > 1000);
+    header.classList.toggle("PassouDaImagem", window.scrollY > 650);
     //passou de 1000px de altura, a classe do header vai para .PassouDaImagem{}
 })
