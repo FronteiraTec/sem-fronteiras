ce6b8bfadd2ee67f8b642ce054c90bb9fceef660
Fabricio Zanotto <fabricioz.1.2@hotmail.com>
2020-10-27 01:14:20 -0300

    adicionando o header, pastas css separadas e sobre empresa

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,44 @@
+var alturaDoBanner = document.querySelector(".imagemBanner").clientHeight;
+console.log(alturaDoBanner)
 window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
-    header.classList.toggle("PassouDaImagem", window.scrollY > 650);
-    //passou de 1000px de altura, a classe do header vai para .PassouDaImagem{}
+    header.classList.toggle("PassouDaImagem", window.scrollY > alturaDoBanner);
 })
+
+var checkbox = document.getElementById("check");
+var minhasopcoesdemenu = document.getElementById("PrecisoParajs");
+
+if(checkbox.checked == false){
+    minhasopcoesdemenu.style.left = "-100%";
+}
+function MeuBotaoMenu(){
+    var checkbox = document.getElementById("check");
+    var minhasopcoesdemenu = document.getElementById("PrecisoParajs");
+
+    if(checkbox.checked == true){
+        minhasopcoesdemenu.style.left = "60vw";
+    }else{
+        minhasopcoesdemenu.style.left = "-100vw";
+    }
+   
+}
+
+const menuItens = document.querySelectorAll('header a[href^="#"]');
+
+menuItens.forEach(item =>{
+    item.addEventListener("click" ,AlteraALturaClick);
+})
+
+function AlteraALturaClick(event){
+    event.preventDefault();
+    const elemento =  event.target;
+    const id = elemento.getAttribute('href');
+    const section = document.querySelector(id).offsetTop;
+    window.scroll({
+        top:section-100,
+        behavior:"smooth",
+    });
+    checkbox.checked = false;
+    minhasopcoesdemenu.style.left = "-100%";
+}
+
